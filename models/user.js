7c440e1723a1ec486bc1e3f5eb239a63c46032a6
Abdul Rehman <mehars.6925@gmail.com>
2020-08-08 18:12:39 +0500

    updated

--- models/user.js
+++ models/user.js
@@ -1,8 +1,12 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose') 
+const Schema = mongoose.Schema 
 
-const userSchema = {
-    name: String,
-    email: String,
-    account_type: String
-}
+const userSchema = new Schema({ // user schema
+    name: String, // To store name of the user
+    email: String, // To store email of the user
+    password: String, // To store password of the user
+    role: String, // it can be "customer" or "seller"
+    stripe_account_id: String // if the user have "seller" role, we need this (stores stripe account_id of the user)
+})
+
+module.exports = mongoose.model('User', userSchema) // Creates model from schema and exports it
