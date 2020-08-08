65e8ce82f7165240bd1082b6bc562e1b1dba75ce
Abdul Rehman <mehars.6925@gmail.com>
2020-08-08 18:11:07 +0500

    deleted the model

--- models/account_information.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const accountInfoSchema = {
-    userId: mongoose.Schema.Types.ObjectId,
-    stripeId: String
-}
-
-export default AccountInfo = mongoose.model('AccountInformation', accountInfoSchema)
